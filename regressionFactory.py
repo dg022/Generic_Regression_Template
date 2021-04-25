b129b7a49efa6a0841ad9eea5ea834b3933bf4f0
David George <44826125+dg022@users.noreply.github.com>
2021-04-25 14:00:01 -0600

    generic polynomial regression working

--- regressionFactory.py
+++ regressionFactory.py
@@ -1,6 +1,11 @@
 from regressionMethods.MultiLinearRegression import  MultiLinearRegression
+from regressionMethods.PolynomialRegression import  PoynomialRegression
 
 
 obj = MultiLinearRegression("Data.csv")
+result = obj.train_model()
+print(result[0])
+
+obj = PoynomialRegression("Data.csv")
 result = obj.train_model()
 print(result[0])
