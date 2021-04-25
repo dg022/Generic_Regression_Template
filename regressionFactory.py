0aa58ef1d805d0956fc005d78502d494df5f4da0
David George <44826125+dg022@users.noreply.github.com>
2021-04-25 17:28:46 -0600

    random forest regression

--- regressionFactory.py
+++ regressionFactory.py
@@ -2,7 +2,7 @@ from regressionMethods.MultiLinearRegression import  MultiLinearRegression
 from regressionMethods.PolynomialRegression import  PoynomialRegression
 from regressionMethods.SupportVectorRegression import SupportVectorRegression
 from regressionMethods.DecisionTreeRegression  import DecisisionTreeRegression
-
+from regressionMethods.RandomForestRegression import RandomForestRegression
 obj = MultiLinearRegression("Data.csv")
 result = obj.train_model()
 print(result[0])
@@ -15,5 +15,9 @@ result = obj.train_model()
 print(result[0])
 
 obj = DecisisionTreeRegression("Data.csv")
+result = obj.train_model()
+print(result[0])
+
+obj = RandomForestRegression("Data.csv")
 result = obj.train_model()
 print(result[0])
