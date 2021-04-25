a706cd8a84b82fa696330efe9c453dfc126bb412
David George <44826125+dg022@users.noreply.github.com>
2021-04-25 17:26:09 -0600

    decision tree regression completed

--- regressionFactory.py
+++ regressionFactory.py
@@ -1,6 +1,7 @@
 from regressionMethods.MultiLinearRegression import  MultiLinearRegression
 from regressionMethods.PolynomialRegression import  PoynomialRegression
 from regressionMethods.SupportVectorRegression import SupportVectorRegression
+from regressionMethods.DecisionTreeRegression  import DecisisionTreeRegression
 
 obj = MultiLinearRegression("Data.csv")
 result = obj.train_model()
@@ -10,5 +11,9 @@ obj = PoynomialRegression("Data.csv")
 result = obj.train_model()
 print(result[0])
 obj = SupportVectorRegression("Data.csv")
+result = obj.train_model()
+print(result[0])
+
+obj = DecisisionTreeRegression("Data.csv")
 result = obj.train_model()
 print(result[0])
---
+++ regressionMethods/DecisionTreeRegression.py
@@ -0,0 +1,24 @@
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.tree import DecisionTreeRegressor
+
+class DecisisionTreeRegression:
+    def __init__(self, fileName, split=True, splitsize=0.2):
+        self.dataset = pd.read_csv(fileName)
+        self.X = self.dataset.iloc[:, :-1].values
+        self.Y = self.dataset.iloc[:, -1].values
+        self.split =  split
+        self.splitsize = splitsize
+
+    def train_model(self):
+        if(self.split):
+            self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(self.X, self.Y, test_size=self.splitsize, random_state=0)
+            regressor = DecisionTreeRegressor(random_state = 0)
+            regressor.fit(self.X_train, self.Y_train)
+            self.Y_pred = regressor.predict(self.X_test)
+            return [r2_score(self.Y_test, self.Y_pred), regressor]
+
+
