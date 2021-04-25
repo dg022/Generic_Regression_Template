86cafc6e285b41e4bf3797b14e919574c9e711a4
David George <44826125+dg022@users.noreply.github.com>
2021-04-25 17:37:26 -0600

    generic template for rapid testing works

--- regressionFactory.py
+++ regressionFactory.py
@@ -3,21 +3,21 @@ from regressionMethods.PolynomialRegression import  PoynomialRegression
 from regressionMethods.SupportVectorRegression import SupportVectorRegression
 from regressionMethods.DecisionTreeRegression  import DecisisionTreeRegression
 from regressionMethods.RandomForestRegression import RandomForestRegression
-obj = MultiLinearRegression("Data.csv")
+obj = MultiLinearRegression("CroqPain.csv")
 result = obj.train_model()
 print(result[0])
 
-obj = PoynomialRegression("Data.csv")
+obj = PoynomialRegression("CroqPain.csv")
 result = obj.train_model()
 print(result[0])
-obj = SupportVectorRegression("Data.csv")
-result = obj.train_model()
-print(result[0])
-
-obj = DecisisionTreeRegression("Data.csv")
+obj = SupportVectorRegression("CroqPain.csv")
 result = obj.train_model()
 print(result[0])
 
-obj = RandomForestRegression("Data.csv")
+obj = DecisisionTreeRegression("CroqPain.csv")
+result = obj.train_model()
+print(result[0])
+
+obj = RandomForestRegression("CroqPain.csv")
 result = obj.train_model()
 print(result[0])
